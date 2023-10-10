7faab5787bec5a01df6255cc66032d1b3356d7c5
DorsalTag <78026291+DorsalTag@users.noreply.github.com>
2023-10-10 15:10:37 +0200

    AR-15
    Anim Events added
    Texture + Mat

---
+++ OperationKillhouse/Assets/Art/Player/Weapons/EventManager.cs
@@ -0,0 +1,40 @@
+using Gun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventManager : MonoBehaviour {
+
+    public Ammocounter Ammocounter;
+    public GunManager GunManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void MagBullet () {
+        Ammocounter.Mag();
+    }
+
+    public void Cassing () {
+        
+    }
+
+    public void Fire () {
+
+    }
+
+    public void Reload () {
+
+    }
+
+
+}

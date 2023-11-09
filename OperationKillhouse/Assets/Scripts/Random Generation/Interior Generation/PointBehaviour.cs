6daa2f85bd5a25720c476dd531523cf7c5051a1a
SilverSkullCustoms <70266927+SilverSkullCustoms@users.noreply.github.com>
2023-11-09 11:21:30 +0100

    Added Targets + clutter & fixes for A -rooms
    Also prefab for start-area

--- OperationKillhouse/Assets/Scripts/Random Generation/Interior Generation/PointBehaviour.cs
+++ OperationKillhouse/Assets/Scripts/Random Generation/Interior Generation/PointBehaviour.cs
@@ -7,9 +7,16 @@ public class PointBehaviour : MonoBehaviour
     public GameObject[] possibleItems;
     [Range(0.00f, 1.00f)]public float spawnChance = .5f;
 
+    public bool alwaysSpawn;
+
     void Start()
     {
-        if (Random.value < spawnChance)
+        if (Random.value < spawnChance && !alwaysSpawn)
+        {
+            int index = Random.Range(0, possibleItems.Length);
+
+            Instantiate(possibleItems[index], transform.position, possibleItems[index].transform.rotation, transform);
+        }else
         {
             int index = Random.Range(0, possibleItems.Length);
 

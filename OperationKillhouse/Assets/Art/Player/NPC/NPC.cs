0f37c6ef73b5de3064c0d2d0e9935dac8869ac27
DorsalTag <78026291+DorsalTag@users.noreply.github.com>
2023-12-19 15:49:35 +0100

    NPC to main scene no texture yet

---
+++ OperationKillhouse/Assets/Art/Player/NPC/NPC.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : MonoBehaviour {
+
+    public Animator nPC;
+    public Animator aR;
+    public Animator pistol;
+     
+    public float timer;
+
+    public AudioSource piew;
+    public AudioSource smalPiew;
+
+    public bool test;
+    void Start() {
+
+        timer = 60f;
+        
+    }
+
+    void Update() {
+
+        timer -= Time.deltaTime;
+
+        if (timer <= 0) {
+            nPC.SetTrigger ("GO");
+            aR.SetTrigger ("GO");
+            pistol.SetTrigger ("GO");
+        }
+
+        if(test == true) {
+            nPC.SetTrigger ("GO");
+            aR.SetTrigger ("GO");
+            pistol.SetTrigger ("GO");
+            test = false;
+        }
+    }
+
+    public void AinimationDone () {
+        timer = 60f;
+    }
+
+    public void Piew () {
+        piew.Play ();
+    }
+
+    public void PiewSmall () {
+        smalPiew.Play ();
+    }
+
+    public void Casing () {
+
+    }
+
+    public void CasingSmall () {
+
+    }
+
+}

2e6ae939e23acba6bba4597b17fbb52231aa0aa3
snoepkanon <78019309+snoepkanon@users.noreply.github.com>
2024-01-18 16:53:33 +0100

    gun range buttons added

--- OperationKillhouse/Assets/TargetButton.cs
+++ OperationKillhouse/Assets/TargetButton.cs
@@ -6,9 +6,15 @@ public class TargetButton : MonoBehaviour
 {
     public GunRangeTarget target;
 
+    public void FixedUpdate()
+    {
+        target.callTargetBool = false;
+        target.moveTargetBackBool = false;
+    }
+
     public void TargetLogic()
     {
-        if (this.name == ("Reset"))
+        if (this.name == ("ResetButton"))
         {
             target.resetAnimation.Play();
         }
